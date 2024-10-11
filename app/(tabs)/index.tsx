addab8e80ddb6a5eb2ade5609991d49fe16c6a19
luocheng <luocheng@wepie.com>
2024-10-11 20:47:58 +0800

    feat: nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import {Href, Link} from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/about" style={styles.button}>
+      <Link href={"about" as Href<string | object>} style={styles.button}>
         Go to About screen
       </Link>
     </View>
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,12 +1,12 @@
 import { View, StyleSheet } from 'react-native';
-import { Link, Stack } from 'expo-router';
+import {Href, Link, Stack} from 'expo-router';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
       <View style={styles.container}>
-        <Link href="/" style={styles.button}>
+        <Link href={"/" as Href<string|object>} style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
